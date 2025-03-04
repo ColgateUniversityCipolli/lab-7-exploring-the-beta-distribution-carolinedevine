f35b7399f12844e525c66b5b7171180b1f69d2e9
carolinedevine <cdevine@colgate.edu>
2025-03-04 13:06:08 -0500

    Task 1 started

---
+++ Lab7Code.R
@@ -0,0 +1,26 @@
+################################################################################
+# Lab 7
+# Caroline Devine
+################################################################################
+
+################################################################################
+# Task 1: Describe The Population Distribution
+################################################################################
+library(tidyverse)
+library(patchwork)
+
+# Goal: Plot 4 distributions, compute mean, variance, skewness, and kurtosis
+# for all 4 cases
+# Write a function for efficiency
+
+
+
+beta.pop.distribution <- function(alpha, beta){
+  beta.distribution <- data.frame(
+    mean = ,
+    variance = ,
+    skewness = ,
+    excess.kurtosis = ,
+  )
+}
+
